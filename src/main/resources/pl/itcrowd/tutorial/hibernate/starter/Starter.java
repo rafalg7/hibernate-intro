9159ab62b895814fea162c91997b4e96d2bd6e9b
Rafal Gielczowski <rafal.gielczowski@itcrowd.pl>
2013-01-21 11:17:57 +0100

    Persist example entities

--- src/main/resources/pl/itcrowd/tutorial/hibernate/starter/Starter.java
+++ src/main/resources/pl/itcrowd/tutorial/hibernate/starter/Starter.java
@@ -1,15 +1,70 @@
 package pl.itcrowd.tutorial.hibernate.starter;
 
+import pl.itcrowd.tutorial.hibernate.domain.Address;
+import pl.itcrowd.tutorial.hibernate.domain.Company;
+import pl.itcrowd.tutorial.hibernate.domain.Department;
+import pl.itcrowd.tutorial.hibernate.domain.Employee;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.logging.Logger;
 
 @Singleton
 @Startup
 public class Starter {
 
+    @PersistenceContext
+    private EntityManager em;
+
+    private static final Logger LOGGER = Logger.getLogger(Starter.class.getCanonicalName());
+
     @PostConstruct
     private void onCreate(){
+
+        Address address = new Address();
+        address.setCity("Tarnow");
+        address.setStreet("Narutowicza");
+        address.setZipCode("33-100");
+
+        Company company = new Company();
+        company.setName("ITCrowd");
+        company.setAddress(address);
+
+
+        Department department1 = new Department();
+        department1.setName("D1");
+        department1.setCompany(company);
+        department1.setAddress(address);
+        Department department2 = new Department();
+        department2.setName("D2");
+        department2.setCompany(company);
+        department2.setAddress(address);
+
+
+        Employee employee3 = new Employee();
+        employee3.setName("M. Smith");
+        employee3.setAddress(address);
+        employee3.setDepartment(department2);
+        Employee employee1 = new Employee();
+        employee1.setName("J. Kowalski");
+        employee1.setAddress(address);
+        employee1.setDepartment(department1);
+        employee1.setBoss(employee3);
+        Employee employee2 = new Employee();
+        employee2.setName("W. Nowak");
+        employee2.setAddress(address);
+        employee2.setDepartment(department1);
+        employee2.setBoss(employee3);
+
+        em.persist(company);
+        em.persist(department1);
+        em.persist(department2);
+        em.persist(employee1);
+        em.persist(employee2);
+        em.persist(employee3);
 
     }
 }
